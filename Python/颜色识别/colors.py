9b1a96552cbf93c616caf7dee5ba6ff829f9f883
MannDonne <79242273+manndonneperish@users.noreply.github.com>
2023-06-25 18:58:35 +0800

    Add files via upload

--- Python/颜色识别/colors.py
+++ Python/颜色识别/colors.py
@@ -1,67 +1,95 @@
-import pandas as pd
 import cv2
 import os
 
-
-os.chdir('颜色识别/')  # 设置当前工作区，根据自己的实际情况填写或选择注释
-
+os.chdir('颜色识别/') # 设置当前工作区，根据自己的实际情况更改
 
 '''
-颜色对应表colors.csv基本格式
-air_force_blue_raf,"Air Force Blue (Raf)",#5d8aa8,93,138,1768
-air_force_blue_usaf,"Air Force Blue (Usaf)",#00308f,0,48,143
-air_superiority_blue,"Air Superiority Blue",#72a0c1,114,160,193
-图片名：colorpic.jpg
+该程序用于识别选定矩形区域的平均rgb值
 '''
 
+# 储存区域坐标和全局变量
+regions = []
 
-def get_color_name(r, g, b):
-    min_diff = 10000
-    color_name = ''
-    for i in range(len(csv_df)):
-        d = abs(r - int(csv_df.loc[i, "R"])) + abs(g - int(csv_df.loc[i, "G"])) + abs(b - int(csv_df.loc[i, "B"]))
-        if d <= min_diff:
-            min_diff = d
-            color_name = csv_df.loc[i, "color_name"]
-    return color_name
-
-
-def click_info(event, x, y, flags, param):
-    # 只处理双击事件
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        global b, g, r, xpos, ypos, clicked
-        xpos = x
-        ypos = y
-        b, g, r = img[y, x]  # 获取b, g, r
-        b = int(b)
-        g = int(g)
-        r = int(r)
-        clicked = True
+# 鼠标回调函数
+def select_region(event, x, y, flags, param):
+    global regions
+    
+    # 单机鼠标左键记录坐标
+    if event == cv2.EVENT_LBUTTONDOWN:
+        regions.append((x, y))
         
+        # 在选定点进行标记
+        cv2.circle(image, (x, y), 5, (0, 255, 0), -1)
+        cv2.imshow('Image', image)
+        
+        # 如果选择两个点，绘制矩形区域
+        if len(regions) == 2:
+            x1, y1 = regions[0]
+            x2, y2 = regions[1]
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            
+            # 获取选中区域的平均rgb值
+            cropped_image = image[min(y1, y2):max(y1, y2), min(x1, x2):max(x1, x2)]
+            average_rgb = calculate_average_rgb(cropped_image)
+            
+            # 将rgb平均值转换为整数
+            average_rgb = (int(average_rgb[0]), int(average_rgb[1]), int(average_rgb[2]))
 
-r = g = b = xpos = ypos = 0
-clicked = False
-img = cv2.imread('colorpic.jpg')  # 读取图片
-index = ["color", "color_name", "hex", "R", "G", "B"]
-csv_df = pd.read_csv('colors.csv', names=index, header=None)  # 读取颜色表
+            # 绘制显示平均rgb值的文本
+            text = f"Average RGB: {average_rgb}"
+            cv2.putText(image, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            
+            # 重置区域
+            regions = []
+        
+        cv2.imshow('Image', image)
 
-cv2.namedWindow('image')
-cv2.setMouseCallback('image', click_info)
+# 将图像重置为原始状态
+def reset_image():
+    global image, regions
+    regions = []
+    image = cv2.imread(image_path)
+    cv2.imshow('Image', image)
+
+# 计算区域的平均rgb值
+def calculate_average_rgb(image):
+    total_rgb = [0, 0, 0]
+    num_pixels = 0
+
+    for row in image:
+        for pixel in row:
+            b, g, r = pixel
+            total_rgb[0] += r
+            total_rgb[1] += g
+            total_rgb[2] += b
+            num_pixels += 1
+
+    average_rgb = [int(total / num_pixels) for total in total_rgb]
+    return average_rgb
+
+# 设置图片路径
+image_path = 'colorpic.jpg'
+
+# 加载图片
+image = cv2.imread(image_path)
+
+# 创建一个窗口并将鼠标回调函数绑定到该窗口
+cv2.namedWindow('Image')
+cv2.setMouseCallback('Image', select_region)
+
+# 显示图像
+cv2.imshow('Image', image)
 
 while True:
-    cv2.imshow("image", img)
-    if clicked:
-        # 绘制显示文字的区域
-        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
-        text = get_color_name(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
-        # 显示文字内容
-        cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
-        # 如果像素点的颜色太偏向于白色,就用黑色来显示文字
-        if r + g + b >= 600:
-            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
-    clicked = False
-    # 点击 esc键
-    if cv2.waitKey(20) & 0xFF == 27:
+    # 等待按键
+    key = cv2.waitKey(1) & 0xFF
+
+    # 按'r'键重置图像
+    if key == ord('r'):
+        reset_image()
+    
+    # 按'esc'停止循环
+    if key == 27:  # '27'为'esc'的ASCII值
         break
 
 cv2.destroyAllWindows()
