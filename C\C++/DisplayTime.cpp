1fd64a09c4f8ce0dd52bf0b8b11704906eb11e72
MannDonne <79242273+manndonneperish@users.noreply.github.com>
2023-07-12 21:20:10 +0800

    Add files via upload

---
+++ C\C++/DisplayTime.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int seconds;
+	cout << "Enter an integer for seconds : ";
+	cin >> seconds;
+	int minutes = seconds / 60;
+	int remainingSeconds = seconds % 60;
+	cout << seconds << " seconds is " << minutes << " minutes and " << remainingSeconds << " seconds " << endl;
+
+	return 0;
+}
