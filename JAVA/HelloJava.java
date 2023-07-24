4dc4661d0a0170a9470049db3ebf8f6d9bc71b57
MannDonne <79242273+manndonneperish@users.noreply.github.com>
2023-07-24 21:20:40 +0800

    Add files via upload

---
+++ JAVA/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+    private static String say = "我要学会你";
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("你好 Java" + say);
+    }
+}
